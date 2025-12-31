d4de92edb5cc8fbcd201fe4cfc49d3153f190014
rohitjangid01 <rohit.devteam@webdaire.com>
2025-12-31 19:29:41 +0530

    backen changes

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,26 @@
-import "./src/config/env.config.js";
+// import "./src/config/env.config.js";
+
+// import app from "./src/app.js";
+// import connectDatabase from "./src/config/database.config.js";
+
+// const PORT = process.env.PORT || 5000;
+
+// const startServer = async () => {
+//   try {
+//     await connectDatabase();
+
+//      app.listen(PORT, () => {
+//       console.log(`Server running on port ${PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("Server not started because DB failed");
+//     // process.exit(1);
+//   }
+// };
+
+// startServer();
 
 import app from "./src/app.js";
-import connectDatabase from "./src/config/database.config.js";
 
-const PORT = process.env.PORT || 5000;
+export default app;
 
-const startServer = async () => {
-  try {
-    await connectDatabase();
-
-     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Server not started because DB failed");
-    // process.exit(1);
-  }
-};
-
-startServer();
--- backend/src/config/database.config.js
+++ backend/src/config/database.config.js
@@ -1,18 +1,48 @@
+// import mongoose from "mongoose";
+
+// // // disable buffering (VERY IMPORTANT)
+// // mongoose.set("bufferCommands", false);
+
+// const connectDatabase = async () => {
+//   try {
+//     console.log("db url ", process.env.DATABASE_URL)
+//     await mongoose.connect(process.env.DATABASE_URL);
+
+//     console.log("Database connected");
+//   } catch (error) {
+//     console.error("Database connection failed:", error.message);
+//     throw error;
+//   }
+// };
+
+// export default connectDatabase;
 import mongoose from "mongoose";
 
-// // disable buffering (VERY IMPORTANT)
-// mongoose.set("bufferCommands", false);
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 const connectDatabase = async () => {
-  try {
-    console.log("db url ", process.env.DATABASE_URL)
-    await mongoose.connect(process.env.DATABASE_URL);
+  if (cached.conn) return cached.conn;
 
-    console.log("Database connected");
-  } catch (error) {
-    console.error("Database connection failed:", error.message);
-    throw error;
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is missing");
   }
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(process.env.DATABASE_URL, {
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+  console.log("Database connected");
+
+  return cached.conn;
 };
 
 export default connectDatabase;
+
+
